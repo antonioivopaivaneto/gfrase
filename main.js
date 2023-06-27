d7d7658b25478c7422425628253328d1c8fe43b4
junior <antonioivo.3@gmail.com>
2023-06-27 13:58:51 -0300

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors= require('cors')
+const app = express()
+const port = 3005
+
+app.use(cors())
+
+const frase = require('./frase.json');
+
+app.get('/frase', (req, res) => {
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(frase))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
